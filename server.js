ebb330bd295249aa473faaf93624d29dcaa93558
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-09-13 11:36:18 -0400

    initial server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
